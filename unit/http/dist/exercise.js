6a23d99268cae1b866780aab6c1c10616b17b535
Yin-Tung <a122774007@gmail.com>
2020-04-19 02:17:13 +0800

    [New] Exercise complete in course

--- unit/http/dist/exercise.js
+++ unit/http/dist/exercise.js
@@ -3,8 +3,12 @@ $(document).ready(function() {
     event.preventDefault()
     // Step 9 and step 10 code goes here
 
-    $.get('./step5')
-
+    $.get('./step5', {
+      fname: $('#ajax-form input[name=fname]').val(),
+      lname: $('#ajax-form input[name=lname]').val(),
+    }, (data) => {
+      $('#ajax-output').text(data)
+    })
     // Step 11 code goes here
   })
 });
